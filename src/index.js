7ef085a496bc82a20dbd3ece1feceb3a29e5b948
hanyelC <hanyel.chamon@gmail.com>
2022-08-15 16:02:11 -0300

    first route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+const { randomUUID } = require('crypto')
 const express = require('express')
 
+const customers = []
+
 const app = express()
+
+
+app.use(express.json())
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body
+
+  const id = randomUUID()
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+  
+  res.status(201).json(id)
+})
 
 app.listen(3333, console.log('listening on port 3333'))
