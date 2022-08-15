d9fc72e2e9d899906f950e062e0987af96391c63
hanyelC <hanyel.chamon@gmail.com>
2022-08-15 16:41:29 -0300

    create middleware

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 const { randomUUID } = require('crypto')
 const express = require('express')
 
-const users = []
-
-const app = express()
-
-
-app.use(express.json())
-
 /**
  * cpf - string
  * name - string
  * id - uuid
  * statement []
  */
+
+const users = []
+
+const app = express()
+
+app.use(express.json())
+
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+
+  const user = users.find(user => user.cpf === cpf)
+
+  if (!user) {
+    return res.status(404).json({ error: 'User not found'})
+  }
+
+  req.user = user
+
+  return next()
+}
 
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
@@ -34,18 +48,10 @@ app.post('/account', (req, res) => {
   res.status(201).send()
 })
 
-app.get('/statement', (req, res) => {
-  const { cpf } = req.headers
-
-  console.log(cpf)
-
-  const user = users.find(user => user.cpf === cpf)
-
-  if (!user) {
-    return res.status(404).json({ error: 'User not found'})
-  }
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+  const { statement } = req.user
   
-  return res.json(user.statement)
+  return res.json(statement)
 })
 
 app.listen(3333, console.log('listening on port 3333'))
